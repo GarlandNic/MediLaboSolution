406bfe0b8af47207e82301774708f530c76ccd93
GarlandNic <nicolas-garland@club-internet.fr>
2024-04-04 13:25:02 +0200

    patient - crud

--- api-patients/src/main/java/com/projet9/patients/controller/PatientController.java
+++ api-patients/src/main/java/com/projet9/patients/controller/PatientController.java
@@ -1,8 +1,14 @@
 package com.projet9.patients.controller;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.projet9.patients.model.Patient;
 import com.projet9.patients.service.PatientService;
 
 @Controller
@@ -11,4 +17,36 @@ public class PatientController {
 	@Autowired
 	PatientService patientServ;
 
+
+    @RequestMapping("/getPatients")
+    public List<Patient> listOfPatients()
+    {
+        return patientServ.listOfPatients();
+    }
+    
+//    @RequestMapping("/getPatientBy") 
+//    public List<Patient> getPatientBy(@RequestParam Map<String, String> param) {
+//    	return patientServ.patientBy(param);
+//    }
+    
+    @RequestMapping("/getPatientById") 
+    public Patient getPatientBy(@RequestParam int id) {
+    	return patientServ.patientById(id);
+    }
+    
+    @RequestMapping("/addPatient")
+    public Patient addPatient(@RequestParam Patient patient) {
+    	return patientServ.create(patient);
+    }
+    
+    @RequestMapping("/updatePatient")
+    public Patient updatePatient(@RequestParam Patient patient) {
+    	return patientServ.update(patient);
+    }
+    
+    @RequestMapping("/deletePatient")
+    public void deletePatient(@RequestParam Patient patient) {
+    	patientServ.delete(patient);
+    }
+    
 }
--- api-patients/src/main/java/com/projet9/patients/service/PatientService.java
+++ api-patients/src/main/java/com/projet9/patients/service/PatientService.java
@@ -1,8 +1,14 @@
 package com.projet9.patients.service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.projet9.patients.model.Patient;
 import com.projet9.patients.repository.PatientRepository;
 
 @Service
@@ -10,6 +16,36 @@ public class PatientService {
 	
 	@Autowired
 	PatientRepository patientRepo;
+
+	public List<Patient> listOfPatients() {
+		List<Patient> result = new ArrayList<>();
+		patientRepo.findAll().forEach(p -> result.add(p));
+		return result;
+	}
+
+//	public List<Patient> patientBy(Map<String, String> param) {
+//		List<Patient> result = new ArrayList<>();
+//		patientRepo.findAll().forEach(p -> result.add(p));
+//		return result;
+//	}
+
+	public Patient patientById(int id) {
+		Optional<Patient> p = patientRepo.findById(id);
+		if(p.isEmpty()) return null;
+		else return p.get();
+	}
+
+	public Patient create(Patient patient) {
+		return patientRepo.save(patient);
+	}
+
+	public Patient update(Patient patient) {
+		return patientRepo.save(patient);
+	}
+
+	public void delete(Patient patient) {
+		patientRepo.deleteById(patient.getId());
+	}
 	
 	
 
