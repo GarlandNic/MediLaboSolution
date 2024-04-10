419a2fd0b4f34b98b91a93dae3c97cc3e20ddadf
GarlandNic <nicolas-garland@club-internet.fr>
2024-04-10 12:36:52 +0200

    gateway

--- medilab-gateway/src/main/java/com/projet9/gateway/MedilabGatewayApplication.java
+++ medilab-gateway/src/main/java/com/projet9/gateway/MedilabGatewayApplication.java
@@ -1,5 +1,7 @@
 package com.projet9.gateway;
 
+import java.util.Base64;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
@@ -8,28 +10,31 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MedilabGatewayApplication {
+	
+	private static final String CREDENTIALS = "user:password";
+	private static final String API_PATIENTS_PATH = "localhost:8091"; 
 
 	public static void main(String[] args) {
 		SpringApplication.run(MedilabGatewayApplication.class, args);
 	}
 	
-
-		@Bean
-		public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
-			return builder.routes()
-				.route("path_route", r -> r.path("/get")
-						.filters(f -> f.addRequestHeader("Hello", "World"))
-						.uri("http://httpbin.org"))
-				.route("circuit_breaker", p -> p
-						.host("*.circuitbreaker.com")
-						.filters(f -> f.circuitBreaker(config -> config.setName("mycmd")))
-						.uri("http://httpbin.org:80")).
-				.route("host_route", r -> r.host("*.myhost.org")
-					.uri("http://httpbin.org"))
-				.route("rewrite_route", r -> r.host("*.rewrite.org")
-					.filters(f -> f.rewritePath("/foo/(?<segment>.*)", "/${segment}"))
-					.uri("http://httpbin.org"))
-				.build();
-		}
+	@Bean
+	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route("path_patients", r -> r.path("/patients/*")
+						.filters(f -> f.rewritePath("/patients", "")
+								.addRequestHeader("Basic", Base64.getEncoder().encodeToString(CREDENTIALS.getBytes())))
+						.uri(API_PATIENTS_PATH))
+//				.route("path_route", r -> r.path("/get")
+//						.filters(f -> f.addRequestHeader("Hello", "World"))
+//						.uri("http://httpbin.org"))
+//				.route("circuit_breaker", p -> p
+//						.host("*.circuitbreaker.com")
+//						.filters(f -> f.circuitBreaker(config -> config
+//								.setName("mycmd")
+//								.setFallbackUri("forward:/fallback"))) //endpoint /fallback à définir dans controller 
+//						.uri("http://httpbin.org:80"))
+			.build();
+	}
 
 }
