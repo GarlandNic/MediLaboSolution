fdccf7ee9f057be209e7f8b3a385fbad12cf3673
GarlandNic <nicolas-garland@club-internet.fr>
2024-04-10 19:46:03 +0200

    debug

--- medilab-front/src/main/java/com/projet9/front/repository/CustomUserRepository.java
+++ medilab-front/src/main/java/com/projet9/front/repository/CustomUserRepository.java
@@ -1,5 +1,15 @@
 package com.projet9.front.repository;
 
-public interface CustomUserRepository {
+import java.util.Optional;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.projet9.front.model.CustomUser;
+
+@Repository
+public interface CustomUserRepository extends CrudRepository<CustomUser, Integer> {
+
+	Optional<CustomUser> findByUsername(String username);
 
 }
--- medilab-front/src/main/java/com/projet9/front/security/CustomUserDetailsService.java
+++ medilab-front/src/main/java/com/projet9/front/security/CustomUserDetailsService.java
@@ -13,34 +13,26 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import com.nnk.springboot.repositories.UserRepository;
+import com.projet9.front.model.CustomUser;
+import com.projet9.front.repository.CustomUserRepository;
 
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
 	
 	@Autowired
-	private UserRepository userRepo;
+	private CustomUserRepository userRepo;
 
-	/** Override the default loadUserByUsername. Get the user from the Database from her username.
-	 * @param the username as a String
-	 * @return a UserDetails object
-	 */
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		Optional<com.nnk.springboot.domain.User> user = userRepo.findByUsername(username);
+		Optional<CustomUser> user = userRepo.findByUsername(username);
 		
 		if(user.isPresent()) {
-			return new User(user.get().getUsername(), user.get().getPassword(), getGrantedAuthorities(user.get().getRole()));
+			return new User(user.get().getUsername(), user.get().getPassword(), getGrantedAuthorities("USER"));
 		} else {
 			throw new UsernameNotFoundException("Invalide email or password");
 		}
 	}
 
-	/**
-	 * This function create the GrantedAuthority object from the role as a String.
-	 * @param the role (user/admin) as a String
-	 * @return a list with one GrantedAuthority object
-	 */
 	private List<GrantedAuthority> getGrantedAuthorities(String role) {
 		List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
 		authorities.add(new SimpleGrantedAuthority("ROLE_" + role));
