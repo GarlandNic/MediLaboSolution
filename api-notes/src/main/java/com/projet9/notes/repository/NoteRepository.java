a947a06eb4d5d2d89f1f6a7e27790969145ccb23
GarlandNic <nicolas-garland@club-internet.fr>
2024-05-06 12:19:43 +0200

    repository notes

---
+++ api-notes/src/main/java/com/projet9/notes/repository/NoteRepository.java
@@ -0,0 +1,12 @@
+package com.projet9.notes.repository;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.projet9.notes.model.Note;
+
+@Repository
+public interface NoteRepository extends MongoRepository<Note, ObjectId> {
+
+}
